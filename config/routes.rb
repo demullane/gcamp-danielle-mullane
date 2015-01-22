dce7ae4d62403aa25367a03ac157965a2f932582
Danielle <danielle@daniellemullane.com>
2015-01-21 17:12:52 -0700

    Add faq class and attributes. Add slugs to FAQ page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-    get '/faq' => 'faq#index'
+    get '/about' => 'about#index'
     get '/terms' => 'terms#index'
+    get '/faq' => 'faq#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
