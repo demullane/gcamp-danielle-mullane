8612bbc319ca85d3383a5cd0aff5f95748b216c2
Danielle <danielle@daniellemullane.com>
2015-02-04 09:49:30 -0700

    Update success notice for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_path, notice: "User was successfully added."
+      redirect_to users_path, notice: "User was successfully created"
     else
       render :new
     end
