f3ea7f1a9854fe7052d5e4de91c2753522d8c421
Danielle <danielle@daniellemullane.com>
2015-02-06 10:31:06 -0700

    Finish CRUD for Projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,23 @@
-class ProjectsController < Application
+class ProjectsController < ApplicationController
+
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "Project was successfully created."
+    else
+      render :new
+    end
   end
 
   def show
@@ -17,18 +27,26 @@ class ProjectsController < Application
   end
 
   def update
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_path, notice: "Project was successfully destroyed."
   end
 
   private
 
   def find_params
-    @user = User.find(params[:id])
+    @project = Project.find(params[:id])
   end
 
   def project_params
+    params.require(:project).permit(:name)
   end
 
 
