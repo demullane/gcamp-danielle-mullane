a69e858f77685e99ae2a8b5dc7693cd38fc65761
Danielle <danielle@daniellemullane.com>
2015-03-02 16:50:59 -0700

    Update task test specs

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -4,18 +4,53 @@ describe 'User can use the tasks page succesfully (CRUD)' do
 
   before :each do
     User.create(first_name: "Joe", last_name: "Student", email: "joestudent@email.com", password: "password")
-    Task.create(description: "This is a test.", task_due_date: "2015-07-28")
+    project = Project.create(name: "Test Project")
+    Task.create(description: "This is a test.", task_due_date: "2015-07-28", project_id: project.id)
 
     visit '/signin'
     fill_in "Email", with: "joestudent@email.com"
     fill_in "Password", with: "password"
     click_button "Sign In"
 
-    visit '/tasks'
+    visit '/projects'
   end
 
+  scenario 'Task total shows on projects index page' do
+
+    expect(page).to have_content("Test Project")
+    expect(page).to have_content(1)
+
+  end
+
+  scenario 'Task total shows on project show page with singular task' do
+
+    click_on "Test Project"
+
+    expect(page).to have_content(1)
+    expect(page).to have_content("Task")
+
+  end
+
+  scenario 'Task total shows on project show page with plural tasks' do
+
+    project_two = Project.create(name: "Second Test Project")
+    Task.create(description: "This is a test for the second test project.", task_due_date: "2016-05-28", project_id: project_two.id)
+    Task.create(description: "This is a second test for the second test project.", task_due_date: "2017-05-18", project_id: project_two.id)
+
+    visit '/projects'
+
+    click_on "Second Test Project"
+
+    expect(page).to have_content(2)
+    expect(page).to have_content("Tasks")
+
+  end
 
   scenario 'User creates a new task' do
+
+    click_on "Test Project"
+
+    click_on "1 Task"
 
     click_on "New Task"
 
@@ -27,13 +62,21 @@ describe 'User can use the tasks page succesfully (CRUD)' do
     click_on "Create Task"
 
     expect(page).to have_content("Task was successfully created.")
-    expect(page).to have_content("Due On: 2015-07-28")
-    expect(page).to have_content("Edit")
+    expect(page).to have_content("2015-07-28")
+    expect(page).to have_content("This is a new task.")
+    expect(page).to have_content("Projects")
+    expect(page).to have_content("Test Project")
+    expect(page).to have_selector("li", :text => "Tasks")
+    expect(page).to have_selector("h1", :text => "Tasks for Test Project")
 
   end
 
   scenario 'User updates a task via tasks index page' do
 
+    click_on "Test Project"
+
+    click_on "1 Task"
+
     click_on "Edit"
 
     fill_in "Description", with: "This is an updated task."
@@ -43,17 +86,25 @@ describe 'User can use the tasks page succesfully (CRUD)' do
 
     expect(page).to have_content("Task was successfully updated.")
     expect(page).to have_content("This is an updated task.")
-    expect(page).to have_content("Due On: 2015-07-28")
-    expect(page).to have_content("Edit")
+    expect(page).to have_content(true)
+    expect(page).to have_content("2015-07-28")
 
   end
 
   scenario 'User updates a task via task show page' do
 
-    click_on "This is a test."
+    click_on "Test Project"
 
-    expect(page).to have_content("This is a test.")
+    click_on "1 Task"
+
+    click_on "Show"
+
+    expect(page).to have_selector("h1", :text => "This is a test.")
     expect(page).to have_content("Due On: 2015-07-28")
+    expect(page).to have_content("Projects")
+    expect(page).to have_content("Test Project")
+    expect(page).to have_selector("li", :text => "Tasks")
+    expect(page).to have_selector("li", :text => "This is a test.")
 
     click_on "Edit"
 
@@ -64,12 +115,16 @@ describe 'User can use the tasks page succesfully (CRUD)' do
 
     expect(page).to have_content("Task was successfully updated.")
     expect(page).to have_content("This is an updated task.")
-    expect(page).to have_content("Due On: 2015-07-28")
-    expect(page).to have_content("Edit")
+    expect(page).to have_content(true)
+    expect(page).to have_content("2015-07-28")
 
   end
 
   scenario 'User deletes a task via tasks index page' do
+
+    click_on "Test Project"
+
+    click_on "1 Task"
 
     click_on "Delete"
 
@@ -105,6 +160,10 @@ describe 'User can use the tasks page succesfully (CRUD)' do
 
   scenario 'User creates a new task with blank description field' do
 
+    click_on "Test Project"
+
+    click_on "1 Task"
+
     click_on "New Task"
 
     click_on "Create Task"
@@ -114,6 +173,10 @@ describe 'User can use the tasks page succesfully (CRUD)' do
   end
 
   scenario 'User edits an existing task and saves with blank description field' do
+
+    click_on "Test Project"
+
+    click_on "1 Task"
 
     click_on "Edit"
 
