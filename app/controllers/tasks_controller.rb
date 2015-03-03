56567365934de06a420b2856c82f0d3e67a556c5
Danielle <danielle@daniellemullane.com>
2015-03-02 17:05:52 -0700

    Remove scaffolding leftovers from tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,44 +23,33 @@ class TasksController < ApplicationController
 
     @task.project_id = params[:project_id]
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to project_tasks_path(@project), notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to project_tasks_path(@project), notice: 'Task was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
     old_attrs = @task.attributes
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to project_tasks_path(@project), notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        # replace blank attributes with old params
-        new_attrs = {}
-        @task.attributes.each do |attr, val|
-          if @task.errors.added? attr, :blank
-            new_attrs[attr] = old_attrs[attr]
-          end
+    if @task.update(task_params)
+      redirect_to project_tasks_path(@project), notice: 'Task was successfully updated.'
+    else
+      # replace blank attributes with old params
+      new_attrs = {}
+      @task.attributes.each do |attr, val|
+        if @task.errors.added? attr, :blank
+          new_attrs[attr] = old_attrs[attr]
         end
-        @task.assign_attributes(new_attrs)
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
+      @task.assign_attributes(new_attrs)
+      render :edit
     end
   end
 
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to project_tasks_path(@project), notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to project_tasks_path(@project), notice: 'Task was successfully destroyed.'
   end
 
   private
