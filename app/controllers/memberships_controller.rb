0d700dc0ac629826198363655ad2a63a00e6105e
Danielle <danielle@daniellemullane.com>
2015-03-09 15:12:50 -0600

    Can successfully add new membership

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,12 +1,39 @@
 class MembershipsController < ApplicationController
+  before_action :authenticate
+  before_action :set_project
 
   def index
     @membership = Membership.new
-    @project = Project.find(params[:project_id])
+    @memberships = @project.memberships
   end
 
-  def new
+  def create
+    @membership = Membership.new(membership_params)
 
+    @membership.project_id = params[:project_id]
+
+    if @membership.save
+      redirect_to project_memberships_path(@project), notice: "#{User.find(@membership.user_id).full_name} was successfully added."
+    else
+      render :index
+    end
   end
+
+  private
+
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
+
+    def membership_params
+      params.require(:membership).permit(:role, :user_id, :project_id)
+    end
+
+    def authenticate
+      redirect_to '/signin' unless current_user
+      if !current_user
+        flash[:notice] = "You must signin first."
+      end
+    end
 
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,10 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  validates :user_id, :role, presence: true
+
+  def member_name
+    "#{user_id.first_name}"
+  end
 end
