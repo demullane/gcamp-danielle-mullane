2d0b3d180efcb865cd1eb776c7843120629ada0a
Danielle <danielle@daniellemullane.com>
2015-03-09 17:28:09 -0600

    Add project member uniqueness validation

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,5 +3,6 @@ class Membership < ActiveRecord::Base
   belongs_to :project
 
   validates :user_id, :role, presence: true
+  validates_uniqueness_of :user_id, :scope => :project_id, message: "has already been added to this project."
 
 end
