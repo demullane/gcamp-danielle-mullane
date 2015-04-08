9334b1c69654a68c9739aff20e8bf0b728dd8229
Danielle <danielle@daniellemullane.com>
2015-04-08 16:51:48 -0600

    Add dependent destroy for comments when user or task is deleted

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,6 @@ class Project < ActiveRecord::Base
   has_many :tasks, :dependent => :destroy
   has_many :memberships, :dependent => :destroy
   has_many :users, through: :memberships
-  has_many :comments
 
   validates :name, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :memberships, :dependent => :destroy
   has_many :projects, through: :memberships
+  has_many :comments, :dependent => :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
