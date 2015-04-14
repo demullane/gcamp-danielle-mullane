a879014424e006f4be6ea4afb34e17216b14301b
Danielle <danielle@daniellemullane.com>
2015-04-14 16:35:47 -0600

    Redirect non-logged in user back to previous page they were on once logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
+  before_action :store_referrer, only: [:new]
+
+  def store_referrer
+    if request.referer == nil
+      session[:return_to] = "/projects/new"
+    else
+      session[:return_to] = request.referer
+    end
+  end
 
   def new
     @user = User.new
@@ -8,7 +17,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user and @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to projects_path, notice: "You have successfully signed in."
+      redirect_to session[:return_to], notice: "You have successfully signed in."
     else
       flash[:error] = "Username / password combination is invalid"
       render :new
