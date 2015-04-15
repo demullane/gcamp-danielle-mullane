c1a0716ae6d83788e87f1ef7bb5dc2b391c953f1
Danielle <danielle@daniellemullane.com>
2015-04-15 16:36:57 -0600

    Fix footer links

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -1,22 +1,41 @@
 require 'rails_helper'
 
-describe 'User cannot view unathorized pages when not signed in' do
+describe 'User cannot view unauthorized pages when not signed in' do
 
-  scenario 'User who is not signed in cannot see Tasks pages' do
-
-    visit "/tasks"
-
-    expect(page).to have_content("You must sign in first.")
-    expect(page).to have_content("Sign into gCamp!")
-  end
-
-  scenario 'User who is not signed in cannot see Projects pages' do
-
+  scenario 'User who is not signed in cannot access /projects' do
     visit "/projects"
 
     expect(page).to have_content("Sign into gCamp!")
     expect(page).to have_content("You must sign in first.")
+  end
 
+
+  scenario 'User who is not signed in cannot access /users' do
+    visit "/users"
+
+    expect(page).to have_content("Sign into gCamp!")
+    expect(page).to have_content("You must sign in first.")
+  end
+
+  scenario 'User who is not signed in cannot access a projects tasks page' do
+    visit "/projects/20/tasks"
+
+    expect(page).to have_content("Sign into gCamp!")
+    expect(page).to have_content("You must sign in first.")
+  end
+
+  scenario 'User who is not signed in cannot access a projects memberships page' do
+    visit "/projects/20/memberships"
+
+    expect(page).to have_content("Sign into gCamp!")
+    expect(page).to have_content("You must sign in first.")
+  end
+
+  scenario 'User who is not signed in cannot access a tasks comment page' do
+    visit "/projects/20/tasks/20"
+
+    expect(page).to have_content("Sign into gCamp!")
+    expect(page).to have_content("You must sign in first.")
   end
 
 end
