32d14ba5789bbac334debec4a2e5bf9a8de0d4a5
Danielle <danielle@daniellemullane.com>
2015-04-16 10:13:26 -0600

    Fix and add new spects for projects

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -17,20 +17,20 @@ describe 'User can use the projects page succesfully (CRUD)' do
 
 
   scenario 'User creates a new project' do
-
-    click_on "New Project"
+    within "#new-project-button-div" do
+      click_link "New Project"
+    end
 
     fill_in "Name", with: "Test Project #2"
 
     click_on "Create Project"
 
     expect(page).to have_content("Project was successfully created.")
-    expect(page).to have_content("Test Project #2")
+    expect(page).to have_content("Tasks for Test Project #2")
     expect(page).to have_content("New Task")
   end
 
   scenario 'User updates a project via projects index page' do
-
     click_on "Edit"
 
     fill_in "Name", with: "Test Project Update"
@@ -41,12 +41,12 @@ describe 'User can use the projects page succesfully (CRUD)' do
     expect(page).to have_content("Test Project Update")
     expect(page).to have_content("Edit")
     expect(page).to have_content("Delete")
-
   end
 
   scenario 'User updates a project via project show page' do
-
-    click_on "Test Project"
+    within "table" do
+      click_on "Test Project"
+    end
 
     expect(page).to have_content("Test Project")
     expect(page).to have_content("Edit")
@@ -62,32 +62,19 @@ describe 'User can use the projects page succesfully (CRUD)' do
     expect(page).to have_content("Test Project Update")
     expect(page).to have_content("Edit")
     expect(page).to have_content("Delete")
-
   end
 
   scenario 'User deletes a project via projects index page' do
-
     click_on "Delete"
 
     expect(page).to have_content("Project was successfully destroyed.")
     expect(page).to_not have_content("Test Project")
-
   end
 
-  #scenario 'User deletes a project via project edit page' do
-
-    #click_on "Edit"
-
-    #click_on "Delete"
-
-    #expect(page).to have_content("Project was successfully destroyed.")
-    #expect(page).to_not have_content("Test Project")
-
-  #end
-
   scenario 'User deletes a project via project show page' do
-
-    click_on "Test Project"
+    within "table" do
+      click_on "Test Project"
+    end
 
     expect(page).to have_content("Test Project")
     expect(page).to have_content("Edit")
@@ -96,22 +83,21 @@ describe 'User can use the projects page succesfully (CRUD)' do
     click_on "Delete"
 
     expect(page).to have_content("Project was successfully destroyed.")
+    expect(page).to have_content("Projects")
     expect(page).to_not have_content("Test Project")
-
   end
 
   scenario 'User creates a new project with blank name field' do
-
-    click_on "New Project"
+    within "#new-project-button-div" do
+      click_link "New Project"
+    end
 
     click_on "Create Project"
 
     expect(page).to have_content("Name can't be blank")
-
   end
 
   scenario 'User edits an existing project and saves with blank name field' do
-
     click_on "Edit"
 
     fill_in "Name", with: ""
@@ -119,7 +105,14 @@ describe 'User can use the projects page succesfully (CRUD)' do
     click_on "Update Project"
 
     expect(page).to have_content("Name can't be blank")
-
   end
 
+  scenario 'When a user deletes a project all related tasks are also deleted' do
+    task1 = Task.create(description: "Task 1 for Test Project", project_id: Project.last.id)
+    task2 = Task.create(description: "Task 2 for Test Project", project_id: Project.last.id)
+
+    click_on "Delete"
+
+    expect(Task.all.length == 0)
+  end
 end
